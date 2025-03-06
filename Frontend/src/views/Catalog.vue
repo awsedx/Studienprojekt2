49d7eb2dc1f0fe767bbab7c874937f447e86292f
wasda <144460739+awsedx@users.noreply.github.com>
2025-03-06 11:42:35 +0100

    added axios
    maybe npm install needs to be run again

--- Frontend/src/views/Catalog.vue
+++ Frontend/src/views/Catalog.vue
@@ -1,11 +1,24 @@
 <template>
-and this is the catalog
+    and this is the catalog
+    <button @click="exampleAxios">le test button</button>
 </template>
 
 <script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
 
+        }
+    },
+    methods: {
+        exampleAxios() {
+            axios.get("https://api.sampleapis.com/coffee/hot").then((response) => {
+                console.log(response);
+            });
+        }
+    }
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
